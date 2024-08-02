f5e87a05f25d14202d0be295e2d4993117c45cd3
SultanTipa1 <josephngumbaugush@gmail.com>
2024-08-02 13:30:51 +0300

    Adds all files

--- src/components/Article.js
+++ src/components/Article.js
@@ -3,8 +3,8 @@ import React from "react";
 function Article() {
   return (
     <div>
-      {/* please pass this test */}
-    </div>
+       <h1>please pass this test</h1>
+       </div>
   );
 }
 
